626e9a5c161df3b3b2255d2e1bab96088a2c00f1
farisfaikar <farisfaikar.r@gmail.com>
2023-09-28 14:38:07 +0700

    feat: implement daisyUI for the components and layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.main')
 
 @section('container')
-  <section class="bg-gray-800">
-    <h1>Library Application Home Page</h1>
-  </section>
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src="{{ asset("img/readiverse-logo-square.png") }}" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Readiverse</h1>
+      <p class="py-6">Readiverse is a library management website where you can browse, borrow, and explore knowledge stored in a universe full of knowledge.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
 @endsection
