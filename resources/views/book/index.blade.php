610b18e90adb844367461658bfdf363275d33fb8
farisfaikar <farisfaikar.r@gmail.com>
2023-10-03 22:04:50 +0700

    frontend: change edit and delete button to use text this is done for accessibility purposes

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -38,16 +38,12 @@
                   <p>{{ $book->category->category_name }}</p>
                 </div>
               </td>
-              <td class="flex pr-0">
-                <a href="{{ route('book.edit', $book) }}" class="btn-edit inline-flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                  </svg>
+              <td class="flex sm:flex-row flex-col pr-0 gap-2">
+                <a href="{{ route('book.edit', $book) }}" class="btn btn-ghost inline-flex w-16 items-center p-2">
+                  Edit
                 </a>
-                <a onclick="delete_modal_{{ $loop->iteration }}.showModal()" class="btn-delete inline-flex items-center cursor-pointer">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2 stroke-error">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                  </svg>
+                <a onclick="delete_modal_{{ $loop->iteration }}.showModal()" class="btn btn-ghost text-error hover:bg-error hover:text-base-100 w-16 inline-flex items-center cursor-pointer p-2">
+                  Delete
                 </a>
               </td>
             </tr>
@@ -62,7 +58,7 @@
                     @method('delete')
                     @csrf
 
-                    <button type="submit" class="btn btn-error">
+                    <button type="submit" class="btn btn-error text-base-100">
                       Delete
                     </button>
                     <button type="button" class="btn btn-ghost" onclick="delete_modal_{{ $loop->iteration }}.close()">
